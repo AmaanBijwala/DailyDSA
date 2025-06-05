0043c86166a33058d045a62916beeacd23295ead
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2025-06-05 17:25:48 +0530

    Adding reverse array implementations

---
+++ src/main/java/BasicMath/ReverseAnArrayWithoutExtraSpace.java
@@ -0,0 +1,25 @@
+package BasicMath;
+import java.util.*;
+
+public class ReverseAnArrayWithoutExtraSpace {
+    public static void main(String[] args) {
+        int a[]={5,4,3,2,1};
+        reverse(a);
+    }
+    static void reverse(int[] a) {
+        int start = 0;
+        int end = a.length-1;
+        while(start<end){
+            int temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+            start++;end--;
+        }c
+        printArray(a);
+    }
+    static void printArray(int [] a){
+        for(int i:a) {
+            System.out.println(i);
+        }
+    }
+}
