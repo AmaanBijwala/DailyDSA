9320a917b664e25ab3e8846dc96d46272bf0bdc7
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2025-06-06 16:22:09 +0530

    Changes in logic for reverse using recursion

--- src/main/java/BasicMath/ReverseArrayUsingRecursion.java
+++ src/main/java/BasicMath/ReverseArrayUsingRecursion.java
@@ -4,11 +4,16 @@ public class ReverseArrayUsingRecursion {
 
     public static void main(String[] args) {
         int a []={5,4,3,2,1};
-        rverseArray(a);
+        reverseArray(a,0,a.length-1);
         printArray(a);
     }
-    static void rverseArray(int[] a){
-
+    static void reverseArray(int[] a,int start ,int end){
+        if(start>end){
+            return;}
+        int temp=a[end];
+        a[end]=a[start];
+        a[start]=temp;
+        reverseArray(a,start+1,end-1);
     }
     static void printArray(int [] a){
         for(int i:a) {
