6c5246ca7cf3369124d0d2e5f2df3c79e3ed9214
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2025-06-06 16:09:13 +0530

    6th June 2025 evrse Array Using Recursion

---
+++ src/main/java/BasicMath/ReverseArrayUsingRecursion.java
@@ -0,0 +1,19 @@
+package BasicMath;
+import java.util.*;
+public class ReverseArrayUsingRecursion {
+
+    public static void main(String[] args) {
+        int a []={5,4,3,2,1};
+        rverseArray(a);
+        printArray(a);
+    }
+    static void rverseArray(int[] a){
+
+    }
+    static void printArray(int [] a){
+        for(int i:a) {
+            System.out.println(i);
+        }
+    }
+
+}
