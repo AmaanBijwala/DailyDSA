016d6248ae403d8e04790bc69cb5e79d52a87ce1
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2025-07-04 21:29:46 +0530

    4th July 2025 Left and Right Sum Differences

--- src/main/java/BasicMath/CheckForPrimeNumber.java
+++ src/main/java/BasicMath/CheckForPrimeNumber.java
@@ -2,7 +2,7 @@ package BasicMath;
 /*
 * You are given an integer n. You need to check if the number is prime or not. Return true if it is a prime number, otherwise return false.
 A prime number is a number which has no divisors except 1 and itself.*/
-public class CheckForPrimeNumber {
+public class  CheckForPrimeNumber {
     public static void main(String[] args) {
         System.out.println(isPrime(3));
     }
