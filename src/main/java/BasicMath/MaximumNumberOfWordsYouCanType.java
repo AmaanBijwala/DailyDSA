12e6ef7d02727ec351884cde1f7371def37890f6
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2025-09-15 22:11:17 +0530

    Find   Maximum Number of Words You Can Type

---
+++ src/main/java/BasicMath/MaximumNumberOfWordsYouCanType.java
@@ -0,0 +1,32 @@
+package BasicMath;
+
+public class MaximumNumberOfWordsYouCanType {
+    public static void main(String[] args)
+    {
+        String text = "hello world", brokenLetters = "ad";
+        System.out.println(canBeTypedWords(text,brokenLetters));
+    }
+
+
+
+        public static int canBeTypedWords(String text, String brokenLetters) {
+            String[] strArr=text.split(" ");
+            int count=0;
+            for(String s:strArr){
+                boolean isWritable=true;
+                for(char b : brokenLetters.toCharArray()){
+                    if(s.indexOf(b)!=-1){
+                        isWritable=false;
+                        break;
+                    }
+                }
+                if(isWritable)
+                    count++;
+            }
+            return count;
+        }
+
+
+
+}
+
