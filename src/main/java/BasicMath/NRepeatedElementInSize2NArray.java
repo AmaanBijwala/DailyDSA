59de9c50506a3af19edb4ab8015fbfed103b6533
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2026-01-02 23:01:01 +0530

    N-Repeated Element in Size 2N Array

---
+++ src/main/java/BasicMath/NRepeatedElementInSize2NArray.java
@@ -0,0 +1,15 @@
+    package BasicMath;
+
+import java.util.*;
+
+public class NRepeatedElementInSize2NArray {
+
+        public int repeatedNTimes(int[] nums) {
+            Set<Integer>s=new HashSet<>();
+            for(int i:nums){
+                if(!s.add(i))
+                    return i;
+            }
+            return 0;
+        }
+    }
