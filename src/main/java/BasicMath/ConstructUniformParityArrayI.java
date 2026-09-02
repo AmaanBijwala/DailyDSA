577b4ce50b984714fcafca25d25c8ec1afb65975
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2026-09-02 23:01:24 +0530

    Construct Uniform Parity Array I

--- src/main/java/BasicMath/ConstructUniformParityArrayI.java
+++ src/main/java/BasicMath/ConstructUniformParityArrayI.java
@@ -1,19 +1,8 @@
 package BasicMath;
+public class ConstructUniformParityArrayI {
 
-import java.util.HashMap;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.Arrays;
 
-public class MinimumDifferenceBetweenHighestAndLowestOfKScores {
-
-    public int minimumDifference(int[] nums, int k) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        int ans = nums[k - 1] - nums[0];
-        for (int i = 0; i + k <= n; i++) {
-            ans = Math.min(ans, nums[i + k - 1] - nums[i]);
-        }
-        return ans;
+    public boolean uniformArray(int[] nums1) {
+        return true;
     }
 }
