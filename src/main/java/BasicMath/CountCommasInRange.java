d5a46e45fda9558effb2aa30ff8a84e3bee05f50
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2026-09-08 23:13:50 +0530

    Count Commas in Range

--- src/main/java/BasicMath/CountCommasInRange.java
+++ src/main/java/BasicMath/CountCommasInRange.java
@@ -1,15 +1,10 @@
 package BasicMath;
-import java.util.*;
-public class FactorialUsingRecursion {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int N = sc.nextInt();
-        System.out.println(fact(N));
-    }
-    static int fact(int N){
-      if(N==0)
-          return 1;
-      return N*fact(N-1);
+
+public class CountCommasInRange {
+
+    public int countCommas(int n) {
+        if(n<1000)
+            return 0;
+        return n-999;
     }
 }
