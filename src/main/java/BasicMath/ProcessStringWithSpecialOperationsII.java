1a49ea7c17b20e6422a1e205d00313d993e491a0
Amaan Bijwala <bijwalaamaan2000@gmail.com>
2026-09-18 23:38:11 +0530

    test

--- src/main/java/BasicMath/ProcessStringWithSpecialOperationsII.java
+++ src/main/java/BasicMath/ProcessStringWithSpecialOperationsII.java
@@ -1,47 +1,45 @@
 package BasicMath;
-import java.util.*;
-public class PathsInMatrixWhoseSumIsDivisibleByK {
-    private static final int MOD = (int) 1e9 + 7;
-    public static void main(String[] args) {
-        int[][] grid={{5,2,4},{3,0,5},{0,7,2}};
-        int k=3;
-        System.out.println(numberOfPaths(grid ,k));
-    }
 
+public class ProcessStringWithSpecialOperationsII {
 
-        public static int numberOfPaths(int[][] grid, int k) {
-            int m = grid.length, n = grid[0].length;
-            int[][] prev = new int[n][k];
-            int[][] curr = new int[n][k];
+    public char processStr(String s, long k) {
+        int n = s.length();
+        long[] len = new long[n];
+        long cur = 0;
 
-            int sum = 0;
-            for (int j = 0; j < n; j++) {
-                sum = (sum + grid[0][j]) % k;
-                prev[j][sum] = 1;
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                cur++;
+            }
+            else if (ch == '*') {
+                if (cur > 0) cur--;
+            }
+            else if (ch == '#') {
+                cur = Math.min(cur * 2, (long)1e15);
             }
 
-            sum = grid[0][0] % k;
-
-            for (int i = 1; i < m; i++) {
-                sum = (sum + grid[i][0]) % k;
-                Arrays.fill(curr[0], 0);
-                curr[0][sum] = 1;
-
-                for (int j = 1; j < n; j++) {
-                    Arrays.fill(curr[j], 0);
-                    int val = grid[i][j];
-
-                    for (int r = 0; r < k; r++) {
-                        int nr = (r + val) % k;
-                        curr[j][nr] = (prev[j][r] + curr[j - 1][r]) % MOD;
-                    }
-                }
-
-                int[][] tmp = prev;
-                prev = curr;
-                curr = tmp;
-            }
-
-            return prev[n - 1][0];
+            len[i] = cur;
         }
+
+        if (k >= cur) return '.';
+
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (len[i] - 1 == k) return ch;
+            }
+            else if (ch == '#') {
+                long prev = len[i] / 2;
+                if (k >= prev) k -= prev;
+            }
+            else if (ch == '%') {
+                k = len[i] - 1 - k;
+            }
+        }
+
+        return '.';
     }
+}
